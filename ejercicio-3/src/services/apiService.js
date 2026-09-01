9e11f2c68d509c1737504b5e0021f0d85c0ea985
Michelle <114263903+michaeltx130@users.noreply.github.com>
2026-09-01 10:20:15 -0700

    ApiService

--- ejercicio-3/src/services/apiService.js
+++ ejercicio-3/src/services/apiService.js
@@ -1,13 +1,22 @@
-export const BASE_URL = "https://jsonplaceholder.typicode.com";
+export const BASE_URL = "https://es.openfoodfacts.org/cgi/search.pl";
 
 export default class ApiService {
+  async getProducts(searchTerm) {
+    const response = await fetch(
+      `${BASE_URL}?search_terms=${searchTerm}&search_simple=1&action=process&json=1&page_size=6`,
+    );
 
-  async getPosts() {
-    const response = await fetch(`${BASE_URL}/posts?_limit=5`);
+    if (!response.ok)
+      throw new Error("No se pudo obtener la información de los productos");
 
-    if(!response.ok) throw new Error("No se pudo obtener la información");
+    const data = await response.json();
 
-    return response.json();
+    return data.products.map((item) => ({
+      id: item.code,
+      title: item.product_name || "Producto sin nombre",
+      description: item.brands || "Marca no especificada",
+      image: item.image_front_small_url || "https://via.placeholder.com/150",
+      meta: item.categories ? item.categories.split(",")[0] : "Alimentos",
+    }));
   }
-
 }
